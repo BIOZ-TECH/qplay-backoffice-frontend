f8b33a19d4b0b293076b7c94df577738b6c93cfc
Aylen <ailu.hoz28@gmail.com>
2022-10-23 23:34:12 -0300

    backup 10

--- src/components/AppLayout/AppSnackbar/index.js
+++ src/components/AppLayout/AppSnackbar/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Alert, Snackbar } from '@mui/material';
 
+import "./styles.css";
+
 const AppSnackBar = ({message}) => {
 
     const [open, setOpen] = useState(false);
@@ -17,9 +19,9 @@ const AppSnackBar = ({message}) => {
   return (
     <>
     { message &&
-    <Snackbar open={open} onClose={handleClose}
+    <Snackbar open={open} onClose={handleClose} 
     anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}>
-    <Alert onClose={handleClose} severity={message.severity} sx={{ width: '100%' }}>
+    <Alert className="snackbar-alert" onClose={handleClose} severity={message.severity} sx={{ width: '100%' }}>
       {message.text}
     </Alert>
   </Snackbar>}
