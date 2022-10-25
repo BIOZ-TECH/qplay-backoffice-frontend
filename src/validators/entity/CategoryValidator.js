af85da33d273c18df7e673419a0205dc120b5358
Aylen <ailu.hoz28@gmail.com>
2022-10-25 17:51:41 -0300

    entity composition

--- src/validators/entity/CategoryValidator.js
+++ src/validators/entity/CategoryValidator.js
@@ -1,26 +1,4 @@
-import StringPropertyComposition from "../StringPropertyComposition";
-import NumberPropertyComposition from "../NumberPropertyComposition";
-import LinkPropertyComposition from "../LinkPropertyComposition";
-
-const properties = {
-    name: new StringPropertyComposition({
-        type: 'string',
-        maximumLength: 30,
-        canBeNull: false,
-    }),
-    position: new NumberPropertyComposition({
-        type: 'number',
-        canBeNull: false,
-        minimum: 1,
-    }),
-    permalink: new LinkPropertyComposition({
-        linkType: 'image',
-    }),
-    description: new StringPropertyComposition({
-        type: 'string',
-        maximumLength: 120,
-    }),
-};
+import CategoryProperties from "./composition/category";
 
 export default class CategoryValidator {
     constructor(entity) {
@@ -35,19 +13,19 @@ export default class CategoryValidator {
     validate() {
         const invalidPropertyMessages = {};
 
-        if(!properties.name.validate(this.name)) {
+        if(!CategoryProperties.name.validate(this.name)) {
             invalidPropertyMessages.name = `Por favor, ingrese un nombre de no más de ${properties.name.maximumLength} carácteres`;
         }
 
-        if(!properties.position.validate(this.position)) {
+        if(!CategoryProperties.position.validate(this.position)) {
             invalidPropertyMessages.position = 'Por favor, ingrese la posición de la categoría';
         }
 
-        if(!properties.permalink.validate(this.permalink)) {
+        if(!CategoryProperties.permalink.validate(this.permalink)) {
             invalidPropertyMessages.permalink = 'Por favor, especifique un enlace de imagen en formato jpg, jpeg o png';
         }
 
-        if(!properties.description.validate(this.description)) {
+        if(!CategoryProperties.description.validate(this.description)) {
             invalidPropertyMessages.description = `Por favor, ingrese una descripción de no más de ${properties.description.maximumLength} carácteres`;
         }
 
