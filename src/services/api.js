1023807c61aa4706a28c99f55e15277f829d4acf
Aylen <ailu.hoz28@gmail.com>
2022-11-15 14:15:15 -0300

    change to productive backend domain

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: `http://localhost:8080/api/`
+  baseURL: `https://qplay-api-gateway.azurewebsites.net/api/`
 });
 
 export default Api;
